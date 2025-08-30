821199f921470ee5646dfc4b156f269ea5b1c045
rizky <rizky05@gmail.com>
2025-08-30 18:00:05 +0700

    Clean up debug files and add nixpacks.toml for proper Laravel deployment

--- public/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "Direct PHP file access works - " . date('Y-m-d H:i:s');
-echo "\nRequest URI: " . ($_SERVER['REQUEST_URI'] ?? 'unknown');
-echo "\nQuery String: " . ($_SERVER['QUERY_STRING'] ?? 'none');
-?>
